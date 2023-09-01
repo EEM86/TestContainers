098e27dd45265b57839b8c4b79836bcefd56bcaf
YY <eevgeniy@gmail.com>
2023-09-01 13:41:55 +0300

    kafka still not working

---
+++ src/test/java/com/example/demo/support/AbstractIntegrationTest.java
@@ -0,0 +1,58 @@
+package com.example.demo.support;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, properties = {
+    "spring.datasource.url=jdbc:tc:postgresql:14-alpine://local/workshop"
+})
+public class AbstractIntegrationTest {
+
+  protected RequestSpecification requestSpecification;
+
+  @LocalServerPort
+  protected int localServerPort;
+
+  static final GenericContainer redis = new GenericContainer("redis:6-alpine")
+      .withExposedPorts(6379);
+
+  static final KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:6.2.1"));
+
+  @DynamicPropertySource
+  public static void configureKafka(DynamicPropertyRegistry registry) {
+    kafka.start();
+  }
+
+
+  @DynamicPropertySource
+  public static void configureRedis(DynamicPropertyRegistry registry) {
+    redis.start();
+    registry.add("spring.data.redis.host", redis::getHost);
+    registry.add("spring.data.redis.port", redis::getFirstMappedPort);
+  }
+
+  @BeforeEach
+  public void setUpAbstractIntegrationTest() {
+    RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+    requestSpecification = new RequestSpecBuilder()
+        .setPort(localServerPort)
+        .addHeader(
+            HttpHeaders.CONTENT_TYPE,
+            MediaType.APPLICATION_JSON_VALUE
+        )
+        .build();
+  }
+
+}
